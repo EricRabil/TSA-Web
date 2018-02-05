78c658af2dc7c26f743c86ca2415d774f003ae66
Eric Rabil <ericjrabil@gmail.com>
2018-02-04 23:15:01 -0500

    Added support for product images on the product listing

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -15,7 +15,7 @@
                         <div class="card-header">
                             {{area.name}}
                         </div>
-                        <img class="card-img" :src="area.products[0].image" :alt="`${area.products[0].name} Image`">
+                        <img class="card-img" :src="area.products[0].image" :alt="`${area.products[0].name} Showcase`">
                     </div>
                 </div>
                 <div class="col-lg-9">
@@ -56,7 +56,7 @@ type Area = {
     identifier: string;
     name: string,
     color: string,
-    shortDescription: string,
+    shortDescription: string | string[],
     products: Product[]
 }
 
@@ -76,7 +76,7 @@ export default Vue.extend({
                          defaultActive: index === 0,
                          name: product.name,
                          identifier: product.identifier,
-                         image: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15fe0f11e86%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15fe0f11e86%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22118.0859375%22%20y%3D%2297.2%22%3E318x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+                         image: (<any>product).image || "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15fe0f11e86%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15fe0f11e86%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22118.0859375%22%20y%3D%2297.2%22%3E318x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
                          description: product.summary,
                          price: product.price,
                      });
